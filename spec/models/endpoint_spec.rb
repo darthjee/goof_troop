bb2c50aff96b1bc893f9f693834b433ad4e36c6c
darthjee <darthjee@gmail.com>
2018-12-18 13:40:10 +0100

    Add specs

--- spec/models/endpoint_spec.rb
+++ spec/models/endpoint_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe Endpoint do
-  xit 'add spec here'
+  subject(:endpoint) { described_class.create(name: name) }
+
+  let(:id)   { endpoint.id.to_s }
+  let(:name) { 'test host' }
+
+  describe '.find' do
+    it 'retrieves document from database' do
+      expect(described_class.find(id)).to eq(endpoint)
+    end
+  end
+
+  describe '#name' do
+    it 'returns the name defined' do
+      expect(endpoint.name).to eq(name)
+    end
+  end
 end
