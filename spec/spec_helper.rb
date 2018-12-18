a08af5c5f9fc03918c74f675d67d35d6ab189fd4
darthjee <darthjee@gmail.com>
2018-12-18 22:06:54 +0100

    Add focus filter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,6 +28,7 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
   config.order = 'random'
   config.infer_spec_type_from_file_location!
+  config.filter_run :focus
 
   config.before do
     Rails.cache.clear
